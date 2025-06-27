f4fac06492c01c5866ec8e943a10b6380b1e3cdb
s24003 <s24003@std.it-college.ac.jp>
2025-06-27 12:03:08 +0900

    課題

--- src/ja/D51Q21kadai.java
+++ src/ja/D51Q21kadai.java
@@ -2,7 +2,7 @@ package ja;
 
 public class D51Q21kadai {
     public static void main(String[] args) {
-        int a = 0100, b = 0x20;
+        int a = 0100, b = 0x100, c = 100;
         System.out.println(a + b);
     }
 }
