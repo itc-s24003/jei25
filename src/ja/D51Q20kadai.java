8b7d117b103ea8a433cc3e84791e2758a93d5328
s24003 <s24003@std.it-college.ac.jp>
2025-06-27 11:26:25 +0900

    課題

---
+++ src/ja/D51Q20kadai.java
@@ -0,0 +1,10 @@
+package ja;
+
+import java.util.Scanner;
+
+public class D51Q20kadai {
+    public static void main(String[] args) {
+        int score = 85;
+        System.out.println((score >= 70) ? "合格" : "不合格");
+    }
+}
