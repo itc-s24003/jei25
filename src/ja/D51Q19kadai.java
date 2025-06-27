fa817d71642357d6954ec52459eaae92917531bb
s24003 <s24003@std.it-college.ac.jp>
2025-06-27 11:12:09 +0900

    課題

---
+++ src/ja/D51Q19kadai.java
@@ -0,0 +1,10 @@
+package ja;
+
+public class D51Q19kadai {
+    public static void main(String[] args) {
+        float f = 4.7f;
+        double d = 0.86;
+        double ans = f * d;
+        System.out.println(ans);
+    }
+}
