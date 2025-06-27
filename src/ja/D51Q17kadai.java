fc728404163a8640422dd2609ec92a6b466be691
s24003 <s24003@std.it-college.ac.jp>
2025-06-27 10:38:12 +0900

    課題

---
+++ src/ja/D51Q17kadai.java
@@ -0,0 +1,14 @@
+package ja;
+
+public class D51Q17kadai {
+    public static void main(String[] args) {
+        int x = 4;
+        if(x < 4){
+            System.out.println("4under");
+        }else if(x > 4){
+            System.out.println("4over");
+        }else{
+            System.out.println("just");
+        }
+    }
+}
