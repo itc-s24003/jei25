197f789534b897749207bffad048f3f4aad52f6c
s24003 <s24003@std.it-college.ac.jp>
2025-06-27 12:28:51 +0900

    課題

---
+++ src/ja/D51Q23kadai.java
@@ -0,0 +1,12 @@
+package ja;
+
+public class D51Q23kadai {
+    public static void main(String[] args) {
+        String[] dt = {"春", "夏", "秋", "冬"};
+        for (String a : dt) {
+            {
+                System.out.print(a);
+            }
+        }
+    }
+}
