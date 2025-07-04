d6e94c68d1b8bd1b6f0be2393728aa74d226bd1d
s24003 <s24003@std.it-college.ac.jp>
2025-07-04 10:51:02 +0900

    課題

---
+++ src/ja/D51Q26kadai.java
@@ -0,0 +1,19 @@
+package ja;
+
+class D51Q26kadai {
+    public static void main(String[] args) {
+        int num = 1;
+
+        switch (num) {
+            case 1:
+                System.out.println("one");
+                break; //switch文を抜ける
+            case 2:
+                System.out.println("two");
+                break;
+            default:
+                System.out.println("other");
+        }
+    }
+}
+
