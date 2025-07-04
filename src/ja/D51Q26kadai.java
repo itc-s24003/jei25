ba764009268a806f9881fd7f3300878a92cfd2bc
s24003 <s24003@std.it-college.ac.jp>
2025-07-04 10:51:46 +0900

    課題

--- src/ja/D51Q26kadai.java
+++ src/ja/D51Q26kadai.java
@@ -4,7 +4,7 @@ class D51Q26kadai {
     public static void main(String[] args) {
         int num = 1;
 
-        switch (num) {
+        switch(num){
             case 1:
                 System.out.println("one");
                 break; //switch文を抜ける
